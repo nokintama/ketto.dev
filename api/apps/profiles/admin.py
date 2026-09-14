a2d0f93d57a8d5d71a4bed94a29465629a7390a5
paxlits <paxlits228@gmail.com>
2026-09-14 18:55:57 +0300

    feat: profiles admin

--- api/apps/profiles/admin.py
+++ api/apps/profiles/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Profile
 
 
+@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'elo', 'matches_played', 'wins', 'losses')
     search_fields = ('user__username',)
